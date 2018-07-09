d24e8f044799ef9958d4d779241ac7d69deb3ea0
mbitfun <syun@mbit.fun>
2018-07-09 17:22:06 +0900

    test

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 //% weight=70 icon="\u30A2" color=#EC7505 block="カタカナ"
-namespace test {
+namespace shimon {
+    let scroll :number
     const hs1 = "!" + '"' + "#$%&'()*+,-./:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`"
     const hs2 = " 0123456789｡｢｣､･ｦｧｨｩｪｫｬｭｮｯｰｱｲｳｴｵｶｷｸｹｺｻｼｽｾｿﾀﾁﾂﾃﾄﾅﾆﾇﾈﾉﾊﾋﾌﾍﾎﾏﾐﾑﾒﾓﾔﾕﾖﾗﾘﾙﾚﾛﾜﾝﾞﾟ"
     const arr = [
@@ -165,8 +166,8 @@ namespace test {
         "0303000000",
         "0205020000"
     ]
-    //% blockId=show_strings block="文字列を表示 %v|スクロール時間 %t"
-    export function showString(text: string, time: number = 500): void {
+    //% blockId=show_strings block="文字列を表示 %v"
+	export function showString(text: string, time: number = 500): void {
         let strings: number[] = [0, 0, 0, 0]
         for (let d = 0; d < text.length; d++) {
             for (let x = 0; x < arr.length; x++) {
@@ -199,12 +200,16 @@ namespace test {
                         tmp % 2 == 1 && led.plot(y - a, b)
                         tmp = (tmp - (tmp % 2)) / 2
                     }
-                basic.pause(time)
+                basic.pause(scroll||time)
                 for (let e = 0; e < 5; e++)
                     for (let f = 0; f < 5; f++)
                         led.unplot(e, f)
             }
         }
 
+    }
+    //% blockId=set_scrollTime block="スクロール時間を設定 %t"
+    export function setScrollTime(time: number): void {
+        scroll = time
     }
 }
