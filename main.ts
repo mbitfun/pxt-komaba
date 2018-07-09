6fb00f14263c2226f9a412fee8861cad7c2f6b7f
mbitfun <syun@mbit.fun>
2018-07-09 17:29:19 +0900

    test

--- main.ts
+++ main.ts
@@ -209,7 +209,9 @@ namespace shimon {
 
     }
     //% blockId=set_scrollTime block="スクロール時間を設定 %t"
-    export function setScrollTime(time: number): void {
+    export function setScrollTime(time: numberi = 500): void {
         scroll = time
     }
 }
+rt function setScrollTime(time: number): void {
+
